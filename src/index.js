4568daa05a1b71a7c291fe814a587d1425fb040f
Jéssica Zanon <jessicatorreszanon@gmail.com>
2023-04-26 17:07:39 -0300

    ultima versao de 26 de abril

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import validator from './validator.js';
 
 console.log(validator,numCard);
 
-const form = document.querySelector('form');
-  const cardNumber = document.querySelector('#cardNumber');
+  document.getElementById("buttonverif").addEventListener("click",validar);
+  function validar(){
+
+  }
+
+  const outside = document.getElementById ('out'); id="buttonverif"
+
+//const form = document.querySelector('form');
+//  const cardNumber = document.querySelector('#cardNumber');
