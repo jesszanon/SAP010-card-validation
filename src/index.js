2634eb2323a428997814216285eec329acbf92bd
Jéssica Zanon <jessicatorreszanon@gmail.com>
2023-05-04 17:06:53 -0300

    última versão

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import validator from './validator.js';
-  //criar um evento para fazer o botão funcionar através do click (Um 'addEventListener' pra ouvir o click)
-document.getElementById("buttonverif").addEventListener("click", validar);
+ 
+document.getElementById("buttonverif").addEventListener("click", validar);  //criar um evento para fazer o botão funcionar através do click (Um 'addEventListener' pra ouvir o click)
 function validar(event) {
-  event.preventDefault()
-  // fazer uma declaração const pra pegar o valor que o usuário vai colocar
-  const numCard = document.getElementById("cardNumber").value;
-  const alert = document.getElementById("resultado");
-  // fazer uma declaração const para puxar o código do validador, tanto o isValid, quanto o maslify
-  const card = validator.isValid(numCard);
-  const masc = validator.maskify(numCard);
+  event.preventDefault() // esse preventDefault é chamado pra evitar que a ação padrão do navegador seja executada.
+  
+  const numCard = document.getElementById("cardNumber").value;  // essa função getElementById() do JavaScript pega o valor de um elemento HTML com o ID "cardNumber" e armazena na variável numCard.
+  const alert = document.getElementById("resultado"); //Aqui essa função foi usada para pegar um elemento HTML com ID=resultado e armazená-lo na variável alert.
+ 
+  const card = validator.isValid(numCard);  // Foi usada para puxar a função isValid do validator e verificar se o número de cartão armazenado na variável numCard é válido.
+  const masc = validator.maskify(numCard); // Foi usada para mascarar o número de cartão armazenado na variável numCard. A função retorna um número de cartão mascarado e armazena o valor na variável masc.
 
-  console.log(validator, numCard);
-  // criar uma condição if e else para mostrar no console as opções do código
+ 
   if (numCard === "") {
-    alert.innerHTML = "Insira o número do cartão";
-  } else if (card === true){
-    alert.innerText = `Cartão ${masc} válido`;
+    alert.innerHTML = "Insira o número do cartão";  // criar uma condição if e else para mostrar no console as opções do código
+  
+  } else if (card === true) {
+    alert.innerText = `Cartão ${masc} válido`; //posso usar a propriedade innerHTML para alterar o conteúdo de um elemento de alerta quando um usuário interage com a página.
   } else {
     alert.innerText = "Cartão Inválido";
 
--- src/validator.js
+++ src/validator.js
@@ -26,7 +26,7 @@ const validator = {
       return false;
     }
   },
-//Primeiro, ele separa o número de cartão em dígitos individuais usando o método split. Em seguida, um loop for é usado para percorrer os elementos do array x, exceto os últimos 4, e atribuir o caractere # para cada elemento. Por último, o método join é usado para juntar os elementos do array em uma string e retornar a string resultante.
+
   maskify: function(card){
     const x = card.split(''); 
     for (let i=0; i< x.length - 4; i++){
